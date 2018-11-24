f0f458b7e9373827c18d9b46a235749ffc9e6690
JohanGJensen <johancentralen@gmail.com>
2018-11-24 14:16:43 +0000

    Updates

---
+++ precache-manifest.23c478508b2b5474f55dd9fbc9f715b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "45c61d8271c740828619",
+    "url": "/ContactManager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "45c61d8271c740828619",
+    "url": "/ContactManager/static/js/main.45c61d82.chunk.js"
+  },
+  {
+    "revision": "1037284670d5f49cd9fd",
+    "url": "/ContactManager/static/css/1.e128daf5.chunk.css"
+  },
+  {
+    "revision": "1037284670d5f49cd9fd",
+    "url": "/ContactManager/static/js/1.10372846.chunk.js"
+  },
+  {
+    "revision": "fba7f2b2bf72dbe84d0c",
+    "url": "/ContactManager/static/js/runtime~main.fba7f2b2.js"
+  },
+  {
+    "revision": "8aa284c56221eb14009747574eab3d80",
+    "url": "/ContactManager/index.html"
+  }
+];
